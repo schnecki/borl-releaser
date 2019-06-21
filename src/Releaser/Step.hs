03710861d453b8c43c35d9f32acb3919df9090de
Manuel Schneckenreither <manuel.schneckenreither@uibk.ac.at>
2019-06-21 16:50:48 +0200

    started runStep

---
+++ src/Releaser/Step.hs
@@ -0,0 +1,18 @@
+
+
+module Releaser.Step
+    ( runStep
+    ) where
+
+import           ML.BORL
+import           SimSim
+
+import           Releaser.Type
+
+
+runStep :: St -> MonadBorl St
+runStep (St ql) = do
+
+  undefined
+
+
--- src/Releaser/Type.hs
+++ src/Releaser/Type.hs
@@ -11,25 +11,37 @@ import           Control.Lens
 import           Data.Serialize
 import           GHC.Generics
 
-import           ML.BORL
-import           SimSim
+import           ML.BORL                   as B
+import           SimSim                    as S
 
+
+type StRep = [Double]
 
 data St = St { _simulation :: BORL SimSim
              -- , _todo
              }
 
-data StSerialisable = StSerialisable { _serSimulation :: BORLSerialisable SimSim
-
+data StSerialisable = StSerialisable { _serSimulation :: BORLSerialisable SimSimSerialisable
                                      } deriving (Generic, Serialize)
 
 
-serializeSt :: St -> BORLSerialisable SimSim
-serializeSt (St sim) = (toSerialisable sim)
+serializeSt :: St -> StSerialisable
+serializeSt (St ql) = StSerialisable (B.toSerialisableWith S.toSerialisable ql)
 
 deserializeSt ::
-     [Action SimSim] -> ActionFilter SimSim -> Decay -> ProxyTableStateGeneraliser SimSim -> ProxyNetInput SimSim -> TensorflowModelBuilder -> StSerialisable -> St
-deserializeSt as aF decay gen inp builder (StSerialisable borl) = St $ (fromSerialisable as aF decay gen inp builder borl)
+     [Action SimSim]
+  -> ActionFilter SimSim
+  -> Decay
+  -> ProxyTableStateGeneraliser SimSim
+  -> ProxyNetInput SimSim
+  -> TensorflowModelBuilder
+  -> Release
+  -> Dispatch
+  -> Shipment
+  -> ProcessingTimes
+  -> StSerialisable
+  -> St
+deserializeSt as aF decay gen inp builder rel disp ship procTimes (StSerialisable ql) = St (B.fromSerialisableWith (S.fromSerialisable rel disp ship procTimes) as aF decay gen inp builder ql)
 
 
 type Releaser = StateT
