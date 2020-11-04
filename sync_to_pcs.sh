dc4898b70f52cf23906360d6d2a253141f262e3d
Manuel Schneckenreither <manuel.schneckenreither@uibk.ac.at>
2020-11-04 12:56:01 +0100

    sync update

--- sync_to_pcs.sh
+++ sync_to_pcs.sh
@@ -12,10 +12,10 @@ fi
 # Sync  & Run
 for PC in $PCS; do
   echo "SYNCING TO $USER@$PC:$DIR"
-  rsync -tarz --del --force --exclude=.git --exclude=results --exclude=.stack-work --exclude=.stack-work.prof --exclude=state* --exclude=costs --exclude=plts --exclude=.state* --exclude=psiValues --exclude=.psiValues  --exclude=episodeLength --exclude=queueLength --exclude=reward $DIR $USER@$PC:$DIR
+  rsync -tar -zz --del --force --exclude=.git --exclude=results --exclude=.stack-work --exclude=.stack-work.prof --exclude=state* --exclude=costs --exclude=plts --exclude=.state* --exclude=psiValues --exclude=.psiValues  --exclude=episodeLength --exclude=queueLength --exclude=reward $DIR $USER@$PC:$DIR
   if [ $? -ne 0 ]; then
       ssh -t $USER@$PC "mkdir -p $DIR 1>/dev/null"
-      rsync -tarz --del --force --exclude=.git --exclude=.stack-work --exclude=state*  --exclude=costs --exclude=plts --exclude=.state* --exclude=psiValues --exclude=.psiValues --exclude=episodeLength --exclude=queueLength --exclude=reward $DIR $USER@$PC:$DIR/
+      rsync -tar -zz --del --force --exclude=.git --exclude=.stack-work --exclude=state*  --exclude=costs --exclude=plts --exclude=.state* --exclude=psiValues --exclude=.psiValues --exclude=episodeLength --exclude=queueLength --exclude=reward $DIR $USER@$PC:$DIR/
   fi
   echo "Synced data via rsync. Result $?"
   if [ $? -eq 0 ]; then
